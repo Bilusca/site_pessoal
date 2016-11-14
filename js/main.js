52e2f118447866ebfcd6900460c2137a9c8e2eac
Lucas A. Vieira <lucas.vieira@ladsistemas.com.br>
2016-11-14 18:01:12 -0200

    Alterações pequenas, pronto pra ir pra outro Rumo

--- js/main.js
+++ js/main.js
@@ -1,13 +1,4 @@
 (function(){
-	'use strict'
-
-	var body = $('body');
-	var intro = $('.intro');
-
-	if(intro.classList.contains('index')){
-		body.style.background = '#3F76A3';
-	}
-
 	/*
 		Functions
 	*/
